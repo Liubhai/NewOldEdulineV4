82c7d16ed158c13e36e79def565cb5e5a5a52442
SmellTime <1158041553@qq.com>
2020-05-08 22:05:32 +0800

    feat: 新增统一的web基类YKTWebView

---
+++ SeitionEduline/ChuYouYun/BasieView/YKTWebView.m
@@ -0,0 +1,21 @@
+//
+//  YKTWebView.m
+//  YunKeTang
+//
+//  Created by smelltime on 2020/5/8.
+//  Copyright © 2020 ZhiYiForMac. All rights reserved.
+//
+
+#import "YKTWebView.h"
+
+@implementation YKTWebView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+    }
+    return self;
+}
+
+@end
